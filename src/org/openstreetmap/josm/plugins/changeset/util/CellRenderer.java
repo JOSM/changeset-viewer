c46e3e4f0aee544fda7af2e06ee81a40a26cc6cc
Vincent Privat <vincent.privat@gmail.com>
2018-09-21 23:36:19 +0200

    fix NPE
    See https://josm.openstreetmap.de/ticket/16758

--- src/org/openstreetmap/josm/plugins/changeset/util/CellRenderer.java
+++ src/org/openstreetmap/josm/plugins/changeset/util/CellRenderer.java
@@ -23,32 +23,34 @@ public class CellRenderer implements ListCellRenderer<ChangesetBeen> {
     @Override
     public Component getListCellRendererComponent(JList<? extends ChangesetBeen> list, ChangesetBeen changesetBeen, int index, boolean isSelected, boolean cellHasFocus) {
         JPanel jPanelChangeset = new JPanel(new GridLayout(2, 1, 8, 8));
-        JPanel jPanelRow = new JPanel(new GridLayout(1, 3, 10, 10));
-        JPanel jPanelNumChanges = new JPanel(new GridLayout(1, 2, 10, 10));
-        jPanelNumChanges.setBackground(Color.BLACK);
-        JLabel jLabelDate = new JLabel(changesetBeen.getDate(), SwingConstants.CENTER);
-        jLabelDate.setForeground(new Color(0, 174, 255));
-//      JLabel jLabelChangesetId = new JLabel(String.valueOf(changesetBeen.getChangesetId()));
-        jPanelChangeset.setBorder(BorderFactory.createTitledBorder("Changeset :" + String.valueOf(changesetBeen.getChangesetId())));
-        JLabel jLabelUser = new JLabel(changesetBeen.getUser(), SwingConstants.CENTER);
-        JLabel jLabelCreate = new JLabel(String.valueOf(changesetBeen.getCreate()), SwingConstants.CENTER);
-        jLabelCreate.setForeground(new Color(50, 214, 184));
-        jLabelCreate.setFont(new Font("Serif", Font.BOLD, 12));
-        JLabel jLabelModify = new JLabel(String.valueOf(changesetBeen.getModify()), SwingConstants.CENTER);
-        jLabelModify.setForeground(new Color(229, 228, 61));
-        jLabelModify.setFont(new Font("Serif", Font.BOLD, 12));
-        JLabel jLabelDelete = new JLabel(String.valueOf(changesetBeen.getDelete()), SwingConstants.CENTER);
-        jLabelDelete.setForeground(new Color(197, 38, 63));
-        jLabelDelete.setFont(new Font("Serif", Font.BOLD, 12));
-//      jPanelRow.add(jLabelChangesetId);
-        jPanelRow.add(jLabelUser);
-        jPanelNumChanges.add(jLabelCreate);
-        jPanelNumChanges.add(jLabelModify);
-        jPanelNumChanges.add(jLabelDelete);
-        jPanelRow.add(jPanelNumChanges);
-        //last
-        jPanelChangeset.add(jLabelDate);
-        jPanelChangeset.add(jPanelRow);
+        if (changesetBeen != null) {
+            JPanel jPanelRow = new JPanel(new GridLayout(1, 3, 10, 10));
+            JPanel jPanelNumChanges = new JPanel(new GridLayout(1, 2, 10, 10));
+            jPanelNumChanges.setBackground(Color.BLACK);
+            JLabel jLabelDate = new JLabel(changesetBeen.getDate(), SwingConstants.CENTER);
+            jLabelDate.setForeground(new Color(0, 174, 255));
+    //      JLabel jLabelChangesetId = new JLabel(String.valueOf(changesetBeen.getChangesetId()));
+            jPanelChangeset.setBorder(BorderFactory.createTitledBorder("Changeset :" + String.valueOf(changesetBeen.getChangesetId())));
+            JLabel jLabelUser = new JLabel(changesetBeen.getUser(), SwingConstants.CENTER);
+            JLabel jLabelCreate = new JLabel(String.valueOf(changesetBeen.getCreate()), SwingConstants.CENTER);
+            jLabelCreate.setForeground(new Color(50, 214, 184));
+            jLabelCreate.setFont(new Font("Serif", Font.BOLD, 12));
+            JLabel jLabelModify = new JLabel(String.valueOf(changesetBeen.getModify()), SwingConstants.CENTER);
+            jLabelModify.setForeground(new Color(229, 228, 61));
+            jLabelModify.setFont(new Font("Serif", Font.BOLD, 12));
+            JLabel jLabelDelete = new JLabel(String.valueOf(changesetBeen.getDelete()), SwingConstants.CENTER);
+            jLabelDelete.setForeground(new Color(197, 38, 63));
+            jLabelDelete.setFont(new Font("Serif", Font.BOLD, 12));
+    //      jPanelRow.add(jLabelChangesetId);
+            jPanelRow.add(jLabelUser);
+            jPanelNumChanges.add(jLabelCreate);
+            jPanelNumChanges.add(jLabelModify);
+            jPanelNumChanges.add(jLabelDelete);
+            jPanelRow.add(jPanelNumChanges);
+            //last
+            jPanelChangeset.add(jLabelDate);
+            jPanelChangeset.add(jPanelRow);
+        }
         return jPanelChangeset;
     }
 }
